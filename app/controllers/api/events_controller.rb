09bfe0e20c5b5b18f6293394dc0f9033d96e09fc
apple <shaurya157@gmail.com>
2017-02-11 19:55:48 -0800

    creating new user from installation

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,8 +1,14 @@
 class Api::EventsController < ApplicationController
-  skip_before_filter  :verify_authenticity_token
+  skip_before_action  :verify_authenticity_token
 
   def create
-    puts 'sup'
+    if params['name'] == 'app.install'
+      info = HTTP.get('https://api.flock.co/v1/users.getInfo', params: {token: params['userToken']}).parse
+      
+
+    elsif params['name'] == 'app.uninstall'
+      puts 'uninstalling'
+    end
     render json: 'Thanks', status: 200
   end
 end
